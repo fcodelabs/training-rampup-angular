3b79128354850a2279b5bea8cd69538d4f98a10e
rashidya <rashmiliyanage2000@gmail.com>
2023-01-31 14:01:33 +0530

    Create data gird with dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { DataGridComponent } from './components/data-grid/data-grid.component'
+import { GridModule } from '@progress/kendo-angular-grid'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HomePageComponent } from './containers/home-page/home-page.component'
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
+	declarations: [AppComponent, DataGridComponent, HomePageComponent],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		GridModule,
+		BrowserAnimationsModule,
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
