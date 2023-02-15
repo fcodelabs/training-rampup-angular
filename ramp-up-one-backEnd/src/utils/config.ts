ae49a26752c1fc4ac4d2120659382bca19b88fa6
DLP-Sekara <dslahirupiyumal99@gmail.com>
2023-02-15 09:47:52 +0530

    removed the backend

--- ramp-up-one-backEnd/src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-export const config = {
-  jwt_secret_key: String(process.env.JWT_ACCESS_KEY),
-  jwt_secretRe_key: String(process.env.JWT_REFRESH_KEY),
-};
